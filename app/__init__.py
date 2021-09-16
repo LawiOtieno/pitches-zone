3fe54499b477969be062945720f854620e269ede
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-09-17 02:57:46 +0300

    modified files

---
+++ app/__init__.py
@@ -0,0 +1,59 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_login import LoginManager
+from flask_simplemde import SimpleMDE
+
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
+simple = SimpleMDE()
+
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong' #provides different security levels.
+login_manager.login_view = 'auth.login'
+
+
+def create_app(config_name):
+
+    # inititalizing application
+    app = Flask(__name__)
+
+ 
+    # creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
+
+
+    #registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
+
+    #setting config
+    from .requests import configure_request
+    configure_request(app)
+
+    # configure UploadSet
+    configure_uploads(app,photos)
+    
+    return app
+
+
+
---
+++ config.py
@@ -0,0 +1,71 @@
+import os
+class Config:
+
+    '''
+    General configuration parent class
+    
+    '''
+
+    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.urandom(32)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+   
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/watchlist_test'
+
+
+
+class DevConfig(Config):
+    
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+
+config_options = {
+
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}    
+
+
+
+
+
+
