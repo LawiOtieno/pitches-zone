c34897550064c106532cb36f78451c6d627077f3
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-09-19 14:28:02 +0300

    add pitches class

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from datetime import datetime
 
 
 @login_manager.user_loader
@@ -49,5 +50,21 @@ class Role(db.Model):
         return f'User {self.name}' 
 
 
- 
-
+class Pitches(db.Model):
+    __tablename__= 'pitches'
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(255))
+    category = db.Column(db.String(255))
+    pitch = db.Column(db.String(255))
+    date = db.Column(db.DateTime(250), default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comments = db.relationship('Comments', backref='title', lazy='dynamic')
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def get_pitches(cls,cate):
+        pitch = Pitches.query.filter_by(category=cate).all()
+        return pitch
+    def __repr__(self):
+        return f"Pitches {self.pitch}','{self.date}')"
