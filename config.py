63035dc25ec94aad0c55e538d5d6151d28e07484
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-09-20 18:29:55 +0300

    minor changes

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ class ProdConfig(Config):
     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
     
-    
+    pass
 
 class DevConfig(Config):
     
